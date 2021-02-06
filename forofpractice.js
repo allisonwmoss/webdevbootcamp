b0657c85e80d6fc87475f7e53cd032e4a74ba1d7
allisonwmoss <76180928+allisonwmoss@users.noreply.github.com>
2021-02-05 16:13:16 -0800

    Create forofpractice.js

---
+++ forofpractice.js
@@ -0,0 +1,13 @@
+const fruits = ['apple', 'orange', 'banana']
+
+for(let sub of fruits) {
+    console.log(sub)
+}
+
+// more practice //
+
+const numbers = [1,2,3,4,5,6,7,8,9];
+undefined
+for(let square of numbers){
+    console.log((square * square))
+}
